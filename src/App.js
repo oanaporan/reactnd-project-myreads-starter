cd3e5bd3f1bf6ccd2e1cbb52e6196264c586758a
Oana-Poran <oanaporan@gmail.com>
2018-11-15 13:22:12 +0200

    Get the books list from the booksAPI and setState(books).

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListShelves from './ListShelves'
 import Search from './Search'
 
 class BooksApp extends React.Component {
   state = {
+    books : []
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    // showSearchPage: false
   }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+  })
+}
 
   render() {
     return (
