6172ddc077ecc0f836c4acc6a0f395ae26e2347e
Oana-Poran <oanaporan@gmail.com>
2018-11-18 13:43:19 +0200

    Shelves array added.

--- src/ListShelves.js
+++ src/ListShelves.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react'
+
 import Shelf from './Shelf'
 
 
 class ListShelves extends Component {
     state = {}
 
+
+
+    updateShelves = () => {
+        const newCurrentlyReading = {
+            name: "Currently Reading",
+            books: this.props.books.filter(book => book.shelf === "currentlyReading")
+        }
+        const newWantToRead = {
+            name: "Want To Read",
+            books: this.props.books.filter(book => book.shelf === "wantToRead")
+        }
+        const newRead = {
+            name: "Read",
+            books: this.props.books.filter(book => book.shelf === "read")
+        }
+        return ([newCurrentlyReading, newWantToRead, newRead]);
+
+    }
+
     render() {
+        let shelves = [];
+        // Check is there are books and array is not empty the update the shelves with books.
+            if(this.props.books && this.props.books.lenght)
+        shelves = this.updateShelves();
+
         return(
             <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-                <div className="bookshelf">
-                <Shelf />
+                <div>
+                {shelves && shelves.map((shelf) => (
+                    <Shelf key={shelf.name} shelf={shelf} 
+                    />))}
                 </div>
             </div>
             <div className="open-search">
