910a968d0c0c45fc9ab919bbac77fd5dbf6d844f
Oana-Poran <oanaporan@gmail.com>
2018-11-18 13:40:40 +0200

    Remove starter comments.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ class BooksApp extends React.Component {
   state = {
     books : [],
     newBook : false
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    // showSearchPage: false
+   
   }
 
   componentDidMount() {
@@ -27,6 +21,7 @@ class BooksApp extends React.Component {
   }
 
   render() {
+
     return (
       <div className="app">
         <Route exact path='/' 
