bf171ec1f137d9f29ca32c4ef38a9c4bd84f98cc
Oana-Poran <oanaporan@gmail.com>
2018-11-19 10:18:59 +0200

    Shelfes render list updated.

--- src/ListShelves.js
+++ src/ListShelves.js
@@ -4,31 +4,29 @@ import Shelf from './Shelf'
 
 
 class ListShelves extends Component {
-    state = {}
-
-
+    state = {
+    }
 
     updateShelves = () => {
         const newCurrentlyReading = {
             name: "Currently Reading",
-            books: this.props.books.filter(book => book.shelf === "currentlyReading")
+            books: this.props.books.filter(book => book.shelf === 'currentlyReading')
         }
         const newWantToRead = {
             name: "Want To Read",
-            books: this.props.books.filter(book => book.shelf === "wantToRead")
+            books: this.props.books.filter(book => book.shelf === 'wantToRead')
         }
         const newRead = {
             name: "Read",
-            books: this.props.books.filter(book => book.shelf === "read")
+            books: this.props.books.filter(book => book.title === 'read')
         }
         return ([newCurrentlyReading, newWantToRead, newRead]);
 
     }
 
     render() {
+
         let shelves = [];
-        // Check is there are books and array is not empty the update the shelves with books.
-            if(this.props.books && this.props.books.lenght)
         shelves = this.updateShelves();
 
         return(
@@ -37,7 +35,7 @@ class ListShelves extends Component {
               <h1>MyReads</h1>
             </div>
             <div className="list-books-content">
-                <div>
+                <div className="bookshelf">
                 {shelves && shelves.map((shelf) => (
                     <Shelf key={shelf.name} shelf={shelf} 
                     />))}
