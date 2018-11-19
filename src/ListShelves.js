3d9840713def3102d20df38f121f03a450bfeab7
Oana-Poran <oanaporan@gmail.com>
2018-11-19 10:53:56 +0200

    Link to search page added.

--- src/ListShelves.js
+++ src/ListShelves.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 import Shelf from './Shelf'
 
@@ -42,7 +43,7 @@ class ListShelves extends Component {
                 </div>
             </div>
             <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            <Link to="/search" >Add a book</Link>
           </div>
           </div>
           
