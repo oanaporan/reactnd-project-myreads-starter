5aa4d7b2cabcb6d765f17a480c0d50f956a4c445
Oana-Poran <oanaporan@gmail.com>
2018-11-28 11:16:20 +0200

    Sate discarted - used  JavaScript destructuring to check value of shelf.

--- src/ChangeShelf.js
+++ src/ChangeShelf.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react'
 
 
 class ChangeShelf extends Component {
-    state = {
-        shelfSelected: this.props.book.shelf || "none"
-    }
+    state = {}
 
     render() {
         return(
             <div className="book-shelf-changer">
                 <select 
-                value={this.state.shelfSelected}
+                value = {this.props.book.shelf ? this.props.book.shelf : none}
                 onChange={(el) => this.props.onChangeShelves(this.props.book, el.target.value)}>
                     <option value="move" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
